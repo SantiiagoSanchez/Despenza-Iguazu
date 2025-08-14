d37f64a4127da9fd2f719df85ba533801c374ec2
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-08-14 19:38:18 -0300

    Empezando a usar Identity

---
+++ DespensaIguazu.Client/Autorizacion/Proveedor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.IO.Pipelines;
+using System.Net;
+using System.Security.Claims;
+
+namespace DespensaIguazu.Client.Autorizacion
+{
+    public class Proveedor : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(4000);
+
+            var NoAutorizado = new ClaimsIdentity();
+            var UserAdmin = new ClaimsIdentity(
+            new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Santiago"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim("DNI", "46.033.394"),
+                new Claim("Alias", "SantiDev")
+            },
+            authenticationType: "Ok"
+            );
+
+
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(UserAdmin)));
+        }
+    }
+}
--- DespensaIguazu.Client/Program.cs
+++ DespensaIguazu.Client/Program.cs
@@ -1,5 +1,7 @@
 using DespensaIguazu.Client;
+using DespensaIguazu.Client.Autorizacion;
 using DespensaIguazu.Client.Servicios;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Web;
@@ -8,6 +10,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, Proveedor>();
 builder.Services.AddScoped<IHttpServicio, HttpServicio>();
 await builder.Build().RunAsync();
