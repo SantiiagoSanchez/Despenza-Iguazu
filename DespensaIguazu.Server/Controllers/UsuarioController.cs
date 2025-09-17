fdd764c8df28c787a97dbc8a52f41d56deb0e23d
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-09-17 15:57:01 -0300

    LogIn y LogOut funcionando

--- DespensaIguazu.Server/Controllers/UsuarioController.cs
+++ DespensaIguazu.Server/Controllers/UsuarioController.cs
@@ -1,87 +1,88 @@
-﻿//using DespensaIguazu.BD.Data.Entity;
-//using DespensaIguazu.Shared.DTO;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.IdentityModel.Tokens;
-//using System.IdentityModel.Tokens.Jwt;
-//using System.Security.Claims;
-//using System.Text;
+﻿using DespensaIguazu.BD.Data.Entity;
+using DespensaIguazu.Shared.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
-//namespace DespensaIguazu.Server.Controllers
-//{
-//    [ApiController]
-//    [Route("api/Usuario")]
-//    public class UsuarioController : ControllerBase
-//    {
-//        private readonly UserManager<IdentityUser> userManager;
-//        private readonly SignInManager<IdentityUser> signInManager;
-//        private readonly IConfiguration configuration;
+namespace DespensaIguazu.Server.Controllers
+{
+    [ApiController]
+    [Route("api/Usuario")]
+    public class UsuarioController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly IConfiguration configuration;
 
-//        public UsuarioController(UserManager<IdentityUser> userManager,
-//                                 SignInManager<IdentityUser> signInManager,
-//                                 IConfiguration configuration)
-//        {
-//            this.userManager = userManager;
-//            this.signInManager = signInManager;
-//            this.configuration = configuration;
-//        }
+        public UsuarioController(UserManager<IdentityUser> userManager,
+                                 SignInManager<IdentityUser> signInManager,
+                                 IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
+        }
 
-//        [HttpPost("registrar")]
-//        public async Task<ActionResult<UserTokenDTO>> CreateUser([FromBody] UserInfoDTO dto)
-//        {
-//            var usuario = new IdentityUser { UserName = dto.Email, Email = dto.Email };
+        [HttpPost("registrar")]
+        public async Task<ActionResult<UserTokenDTO>> CreateUser([FromBody] UserInfoDTO dto)
+        {
+            var usuario = new IdentityUser { UserName = dto.Email, Email = dto.Email };
 
-//            var resultado = await userManager.CreateAsync(usuario, dto.Password);
+            var resultado = await userManager.CreateAsync(usuario, dto.Password);
 
-//            if (resultado.Succeeded)
-//            {
-//                return await ConstruirToken(dto);
-//            }
-//            else
-//            {
-//                return BadRequest(resultado.Errors.First());
-//            }
-//        }
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(dto);
+            }
+            else
+            {
+                return BadRequest(resultado.Errors.First());
+            }
+        }
 
-//        [HttpPost("login")]
-//        public async Task<ActionResult<UserTokenDTO>> Login([FromBody] UserInfoDTO dto)
-//        {
-//            var resultado = await signInManager.PasswordSignInAsync(dto.Email, dto.Password, isPersistent: false, lockoutOnFailure: false);
-//            if (resultado.Succeeded)
-//            {
-//                return await ConstruirToken(dto);
-//            }
-//            else
-//            {
-//                return BadRequest("Login incorrecto");
-//            }
-//        }
+        [HttpPost("login")]
+        public async Task<ActionResult<UserTokenDTO>> Login([FromBody] UserInfoDTO dto)
+        {
+            var resultado = await signInManager.PasswordSignInAsync(dto.Email, dto.Password, isPersistent: false, lockoutOnFailure: false);
+            if (resultado.Succeeded)
+            {
+                return await ConstruirToken(dto);
+            }
+            else
+            {
+                return BadRequest("Login incorrecto");
+            }
+        }
 
-//        public async Task<ActionResult<UserTokenDTO>> ConstruirToken(UserInfoDTO userInfo)
-//        {
-//            var claims = new List<Claim>()
-//            {
-//                new Claim(ClaimTypes.Email, userInfo.Email),
+        public async Task<ActionResult<UserTokenDTO>> ConstruirToken(UserInfoDTO userInfo)
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Email, userInfo.Email),
+                new Claim(ClaimTypes.Name, userInfo.Email)
 
-//            };
-//            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]!));
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]!));
 
-//            var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-//            var expiracion = DateTime.UtcNow.AddMonths(1);
+            var expiracion = DateTime.UtcNow.AddMonths(1);
 
-//            var token = new JwtSecurityToken(
-//                issuer: null,
-//                audience: null,
-//                claims: claims,
-//                expires: expiracion,
-//                signingCredentials: credenciales
-//                );
-//            return new UserTokenDTO()
-//            {
-//                Token = new JwtSecurityTokenHandler().WriteToken(token),
-//                Expiration = expiracion
-//            };
-//        }
-//    }
-//}
+            var token = new JwtSecurityToken(
+                issuer: null,
+                audience: null,
+                claims: claims,
+                expires: expiracion,
+                signingCredentials: credenciales
+                );
+            return new UserTokenDTO()
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiracion
+            };
+        }
+    }
+}
