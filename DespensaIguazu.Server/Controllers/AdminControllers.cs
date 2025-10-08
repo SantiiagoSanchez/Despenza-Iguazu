c1b5c1a01a09ffa5ace481f651052892b1ea78f9
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-10-08 12:11:08 -0300

    Lista Usuarios

---
+++ DespensaIguazu.Server/Controllers/AdminControllers.cs
@@ -0,0 +1,34 @@
+﻿using DespensaIguazu.BD.Data;
+using DespensaIguazu.Shared.DTO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DespensaIguazu.Server.Controllers
+{
+    [ApiController]
+    [Route("usuarios/Admin")]
+    public class AdminControllers : ControllerBase
+    {
+        private readonly Context context;
+        private readonly SignInManager<IdentityUser> userManager;
+
+        public AdminControllers(Context context, SignInManager<IdentityUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<UserListadoDTO>>> Get()
+        {
+            var user = context.Users.AsQueryable();
+            var userDTO = user.Select(x => new UserListadoDTO
+            { 
+                Id = x.Id,
+                Email = x.Email!
+            }).ToList();
+
+            return userDTO;
+        }
+    }
+}
---
+++ DespensaIguazu.Shared/DTO/UserListadoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DespensaIguazu.Shared.DTO
+{
+    public class UserListadoDTO
+    {
+        public string Id { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
